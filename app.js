8a09e5050740cad1d640432ef8fbff5388acc807
Marco <marcosparv@live.com.ar>
2022-02-10 19:40:54 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")));
+app.post('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
